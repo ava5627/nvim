aba46afeff8736896d85a3cb1e550611aeafd950
ava5627 <avasharris1@gmail.com>
2024-02-27 14:46:57 -0600

    fix toggleterm keybindings

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -24,12 +24,12 @@ return {
     keys = function()
         local python = require("toggleterm.terminal").Terminal:new({ cmd = "ipython3", hidden = true, direction = "float" })
         return {
-            { "<C-t>p", function() python:toggle() end, desc = "Toggle ipython terminal" },
-            { "<esc>",  [[<C-\><C-n>]],                 desc = "Close terminal",         mode = "t" },
-            { "<C-h>",  [[<C-\><C-n><C-W>h]],           desc = "Move left",              mode = "t" },
-            { "<C-j>",  [[<C-\><C-n><C-W>j]],           desc = "Move down",              mode = "t" },
-            { "<C-k>",  [[<C-\><C-n><C-W>k]],           desc = "Move up",                mode = "t" },
-            { "<C-l>",  [[<C-\><C-n><C-W>l]],           desc = "Move right",             mode = "t" },
+            { "<C-t>p", function() python:toggle() end, desc = "Toggle ipython terminal", mode = { "n", "t" } },
+            { "<esc>",  [[<C-\><C-n>]],                 desc = "Close terminal",          mode = "t" },
+            { "<C-h>",  [[<C-\><C-n><C-W>h]],           desc = "Move left",               mode = "t" },
+            { "<C-j>",  [[<C-\><C-n><C-W>j]],           desc = "Move down",               mode = "t" },
+            { "<C-k>",  [[<C-\><C-n><C-W>k]],           desc = "Move up",                 mode = "t" },
+            { "<C-l>",  [[<C-\><C-n><C-W>l]],           desc = "Move right",              mode = "t" },
             "<C-esc>"
         }
     end
