0bb5ecba76bbfebbde50d0a1b1543631449e1cbb
ava5627 <avasharris1@gmail.com>
2024-07-26 14:11:09 -0500

    auto indent

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -268,10 +268,14 @@ lazy.setup({
         lazy = false,
         config = true,
         keys = {
-            { "<leader>gb", "<cmd>BlameToggle<CR>", desc = "Git Blame" },
+            { "<leader>gb", "<cmd>BlameToggle<CR>",         desc = "Git Blame" },
             { "<leader>gB", "<cmd>BlameToggle virtual<CR>", desc = "Git Blame Virtual Text" }
         }
-    }
+    },
+    {
+        "Darazaki/indent-o-matic",
+        config = true,
+    },
 }, {
     dev = {
         path = "~/repos",
