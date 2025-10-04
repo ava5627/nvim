918231dd49bdedb10cd048e44e1d92565aa5a14b
ava5627 <avasharris1@gmail.com>
2025-10-04 13:00:00 -0500

    fix tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -63,5 +63,10 @@ return {
             end,
             desc = "Toggle file explorer"
         }
-    }
+    },
+    lazy = false,
+    init = function()
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+    end
 }
